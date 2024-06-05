28f336dfa1423dc4a64771582f5ce9cc681daebd
Vidya <vidya.nk07@gmail.com>
2024-06-06 00:28:53 +0530

    aside section

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,14 @@ items.forEach(item => {
     `
     <div class="col-lg-3 col-md-3 col-sm-4 col-6 g-xl-4 g-lg-3 g-md-2 g-sm-1 g-0">
       <div class="card rounded-0 border-0 shadow-0 h-100 m-lg-3">
-        <img src="${item.image}" class="car-img-top rounded-0">
+        <div class = "cardImage position-relative">
+          <img src="${item.image}" class="card-img-top rounded-0">
+          <div class="w-100 mt-1 mx-auto">
+            <button class="btn btn-outline-danger w-100 rounded-0" onclick="addToCart(${item.id})" id="addcart">
+              Add to Cart
+            </button>
+          </div>
+        </div>
         <div class="card-body">
           <h6 class="card-title fw-bold mb-0">${item.company}</h6>
           <p class="card-text fs-14 fw-400 mb-0">${item.title}</p>
@@ -16,11 +23,7 @@ items.forEach(item => {
             <del class="fs-14 fw-400 me-1">${item.price}</del>
             <span class="text-orange fs-14 fw-400 me-1">${item.discount} off</span>
           </div>
-          <div class="w-100 mt-1 mx-auto">
-            <button class="btn btn-outline-danger w-100" onclick="addToCart(${item.id})">
-              <i class="fa-regular fa-heart fs-6"></i> &nbsp; wishlist
-            </button>
-          </div>
+          
         </div>
       </div>
     </div>
